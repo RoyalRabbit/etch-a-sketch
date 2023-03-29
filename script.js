c773a33cc904c78af1d511e5b958e1ebb424e130
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-29 14:12:41 -0400

    Add event listener inside of grid creation loop

--- script.js
+++ script.js
@@ -3,40 +3,44 @@ console.log(gridContainer)
 
 
 const gridWidth = gridContainer.clientWidth
-console.log(gridWidth)
 
 // Calculate how many grid squares need to be created
-const squaresInRow = 10
+const squaresInRow = 64
 const squares = squaresInRow**2
+
+//Calculate dimensions of squares
+const squareLength = gridWidth / (squaresInRow + 0.3);
 
 
 // Create div elements with class grid in square
 for (let i = 0; i <squares; i++) {
     const grid = document.createElement('div')
     grid.className='grid'
-    grid.style.width = `${gridWidth/ (squaresInRow+.3)}px`;
-    grid.style.height = `${gridWidth/ (squaresInRow+.3)}px`;
-    console.log(grid.style.width)
+    grid.addEventListener('mouseover', obj=>{
+        changeBackgroundRandom(obj);
+    })
+    grid.style.width = `${squareLength}px`;
+    grid.style.height = `${squareLength}px`;
     gridContainer.appendChild(grid);
 }
 
 // Select all grids
 
-let gridAll = document.querySelectorAll('.grid');
+// let gridAll = document.querySelectorAll('.grid');
 
-// Put event listener on each item in gridAll
-gridAll.forEach(item=>{
-    // Mouse over event
-    item.addEventListener('mouseover', obj=>{
-        changeBackgroundRandom(obj);
-    })
+// // Put event listener on each item in gridAll
+// gridAll.forEach(item=>{
+//     // Mouse over event
+//     item.addEventListener('mouseover', obj=>{
+//         changeBackgroundRandom(obj);
+//     })
 
     // Mouse out event
     // item.addEventListener('mouseout', obj=> {
     //     changeBackgroundWhite(obj)
     //     console.log(obj)
     // })
-})
+// })
 
 
 
