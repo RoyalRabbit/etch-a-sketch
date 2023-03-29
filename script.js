1008eaf5cc3505f1afc04c62ee9f93eb361d10c6
RoyalRabbit <122888749+RoyalRabbit@users.noreply.github.com>
2023-03-29 13:30:45 -0400

    Add foreach

--- script.js
+++ script.js
@@ -5,36 +5,32 @@ console.log(gridContainer)
 const gridWidth = gridContainer.clientWidth
 console.log(gridWidth)
 
-
+// Calculate how many grid squares need to be created
 const squaresInRow = 5
 const squares = squaresInRow**2
 
+
+// Create div elements with class grid in square
 for (let i = 0; i <squares; i++) {
     const grid = document.createElement('div')
     grid.className='grid'
     grid.style.width = `${gridWidth/ (squaresInRow+.3)}px`;
     grid.style.height = `${gridWidth/ (squaresInRow+.3)}px`;
+    grid.innerText = i;
     console.log(grid.style.width)
     gridContainer.appendChild(grid);
 }
 
-document.querySelector('.grid').addEventListener('mouseover',(obj)=>{
-    obj.target.classList.add('hovered');
-    const hovered = document.querySelector('.hovered');
-    hovered.style.backgroundColor = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
-    // setTimeout((),300);
-    // setTimeout(()=>{obj.target.classList.remove('hover');}, 300);
+// Select all grids
+
+let gridAll = document.querySelectorAll('.grid');
+
+gridAll.forEach(item=>{
+    addEventListener('mouseenter', obj=>{
+        console.log(obj)
+    })
 })
 
-document.querySelector('.grid').addEventListener('mouseout',(obj)=>{
-
-    // obj.target.classList.remove('hover');
-    setTimeout(()=>{
-        const hovered = document.querySelector('.hovered');
-    hovered.style.backgroundColor = `white`;
-        obj.target.classList.remove('hovered');
-    }, 300);
-})
 
 
 
